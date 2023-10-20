ef00096137d94a02f79849edd6fc70558a547950
koolkaka <82873283+koolkaka@users.noreply.github.com>
2023-10-21 00:41:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Khánh Chi';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Khánh Chi';
+const contentCard = 'Chúc bạn luôn xinh đẹp , vui tươi , yêu đời và thành công. Chúc mọi điều may mắn sẽ đến với bạn.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'khanhchi.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
